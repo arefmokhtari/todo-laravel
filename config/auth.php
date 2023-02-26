b39e03e6dc47697a1a4c53a65a552272e09decd7
arefmokhtari <aref100am@gmail.com>
2023-02-26 09:58:13 +0330

    add admin (m,c,r) & user resources

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,14 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'user' => [
+            'driver' => 'sanctum',
+            'provider' => 'users'
+        ],
+        'admin' => [
+            'driver' => 'sanctum',
+            'provider' => 'admins'
+        ],
     ],
 
     /*
@@ -64,7 +72,10 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
-
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
