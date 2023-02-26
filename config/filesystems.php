0a90ef427e075c357d2ea20280335308d2f9c8e2
arefmokhtari <aref100am@gmail.com>
2023-02-26 09:42:53 +0330

    add redioCrud again

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,6 +45,7 @@ return [
             'root' => storage_path('app'),
             'throw' => false,
         ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
